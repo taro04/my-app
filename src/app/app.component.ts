09a5212c2f2c1fc4d7c313512f2aa662618827bd
taro04 <75844498+taro04@users.noreply.github.com>
2021-10-04 21:54:17 +0900

    feat:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { cosmosclient, rest, proto } from 'cosmos-client';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, combineLatest, observable, Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,67 +10,69 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
 
-  constructor(
-    private http: HttpClient,
-  ) {
-    this.sdk = new cosmosclient.CosmosSDK(this.nodeURL,this.chainID)
-    this.address_from_nemonic().then()
-  }
-
-  sdk: cosmosclient.CosmosSDK
+  //定義
   nodeURL = 'http://localhost:1317'
   chainID = "mars"
-  nemonic = 'sword heart bridge bulk wonder furnace cube window purity rich convince panda bag solution vocal voice group depth donate margin call match swap defy'
-  address = "cosmos1z9gwnnwxdp6qfvg3hekmlqtvju0npm2u6yttw7" //Aliceのaddress
+  address = "cosmos1z9gwnnwxdp6qfvg3hekmlqtvju0npm2u6yttw7" // default_address_(Alice)
 
-  //デフォルトアドレス枠
-  privKey! :proto.cosmos.crypto.secp256k1.PrivKey
-  pubKey! :cosmosclient.PubKey
-  fromAddress! :cosmosclient.AccAddress
-  account! :proto.cosmos.auth.v1beta1.BaseAccount
-  denoms:any
+  //cosmos-sdk.service
+  sdk$: Observable<cosmosclient.CosmosSDK>;
+  restURL$ = new BehaviorSubject(this.nodeURL);
+  chainID$ = new BehaviorSubject(this.chainID);
 
-  //send
-  amount = 0
-  denom = "stake"
-  toAd = "cosmos1rjh23wvj3uqy7p9acuapcr356rc9dmvfs38yf5"
+  //
+  addr$ = new BehaviorSubject(this.address);
+  address$: Observable<cosmosclient.AccAddress>;
+  account$: Observable<proto.cosmos.auth.v1beta1.BaseAccount | unknown | undefined>;
+  balances$: Observable<proto.cosmos.base.v1beta1.ICoin[]>;
 
+  constructor() {
 
-  //デフォルトアドレスをニーモニックから取得(constractor)
-  address_from_nemonic = async (): Promise<void> => {
-    this.privKey = new proto.cosmos.crypto.secp256k1.PrivKey({
-      key: await cosmosclient.generatePrivKeyFromMnemonic(this.nemonic),
-    });
-    this.pubKey = this.privKey.pubKey();
-    this.fromAddress = cosmosclient.AccAddress.fromPublicKey(this.pubKey);
+    //sdkのObservable
+    this.sdk$ = combineLatest([this.restURL$, this.chainID$]).pipe(
+      map(([restURL, chainID]) => ( new cosmosclient.CosmosSDK(restURL, chainID)))
+    );
+    //addressのObservable
+    this.address$ = this.addr$.pipe(
+      map((addr) => cosmosclient.AccAddress.fromString(addr)),
+    );
+    //sdk+address
+    const combined$ = combineLatest([this.sdk$, this.address$]);
+    //res->でaddressのObservable
+    this.account$ = combined$.pipe(
+      mergeMap(([sdk, address]) =>
+        rest.cosmos.auth
+          .account(sdk, address)
+          .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account))
+          .catch((_) => {
+            console.log("kokode err")
+            console.error(_);
+            return undefined;
+          }),
+      ),
+    );
+    //関数でbalancesのObservable
+    this.balances$ = combined$.pipe(
+      mergeMap(([sdk, address]) =>
+        rest.cosmos.bank.allBalances(sdk, address).then((res) => res.data.balances || []),
+      ),
+    );
   }
 
-
-  // get account info
-  get_account_info = async (Adr:string): Promise<void> =>{
-    const address = cosmosclient.AccAddress.fromString(Adr)
-    const _account = await rest.cosmos.auth
-    .account(this.sdk, address)
-    .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account))
-    .catch((_) => undefined);
-    if (!(_account instanceof proto.cosmos.auth.v1beta1.BaseAccount)) {
-      console.log("can't get account",_account);
-      return;
-    }else{
-      console.log("get account");
-      console.dir(_account);
-    }
-    this.account = _account
-    this.getBalance(_account.address.toString())
+  //更新
+  changeAddress(str:string):void{
+    this.addr$.next(str)
   }
-
-  getBalance(address :string):void{
-    const url = `${this.nodeURL}/cosmos/bank/v1beta1/balances/${address}`
-    this.http.get<string>(url)
-    .subscribe( jsonfile => {
-          console.dir(jsonfile)
-          this.address = address
-          this.denoms = JSON.parse(JSON.stringify(jsonfile))["balances"] //ngForで表示する配列。
-        });
-      }
 }
+
+/*
+getBalance(address :string):void{
+  const url = `${this.nodeURL}/cosmos/bank/v1beta1/balances/${address}`
+  this.http.get<string>(url)
+  .subscribe( jsonfile => {
+        console.dir(jsonfile)
+        this.address = address
+        this.denoms = JSON.parse(JSON.stringify(jsonfile))["balances"] //ngForで表示する配列。
+      });
+    }
+*/
