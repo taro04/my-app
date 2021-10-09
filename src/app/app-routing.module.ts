5d06e66eda4219c6309ee766ddd146d95e650f03
taro04 <75844498+taro04@users.noreply.github.com>
2021-10-09 16:33:31 +0900

    fix_parent-child_inv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ViewHomeComponent } from './views/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: ViewHomeComponent,
+    component: HomeComponent,
   },
   {
     path: 'accounts/:address',
