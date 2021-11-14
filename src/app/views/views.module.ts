58962da90d4ef3c543e46d9b4c6818a005e7e0bb
taro04 <75844498+taro04@users.noreply.github.com>
2021-11-14 21:36:17 +0900

    save

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { Link1Component } from './link1/link1.component';
+import { Link3Component } from './link3/link3.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Link1Component,
+    Link3Component
+  ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
   //exports: [AppComponent],
 })
